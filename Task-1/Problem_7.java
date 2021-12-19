e0e02e7b94cfbbe9deb0d265e326e5cc53ec6403
HodaElnaghy <96389841+HodaElnaghy@users.noreply.github.com>
2021-12-20 01:27:10 +0200

    Add files via upload

---
+++ Task-1/Problem_7.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package problem_7;
+
+import java.util.Scanner;
+
+public class Problem_7 {
+
+  
+    public static void main(String[] args) {
+       System.out.println("Enter a number the sum of the numbers from 1 to n");
+        int n, sum=0;  
+        Scanner sc=new Scanner(System.in);  
+        n=sc.nextInt();   
+        if(n>0){
+            for(int i=1 ;i<=n;i++)
+            if (i%3==0 || i%5==0)
+            sum +=i;
+        System.out.println("Sum = " + sum);
+            
+        }
+    }
+    
+}
